60caef0d45346f80cb63d6d2084993c0176795f5
suga935 <33885130+suga935@users.noreply.github.com>
2018-02-05 10:33:41 +0800

    BAAM

--- Log.cpp
+++ Log.cpp
@@ -1,47 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Log
 {
-public:
-	const int LogLevelError=0;
-	const int LogLevelWarning=1;
-	const int LogLevelInfo=2;
-
-private:
-	int m_LogLevel=LogLevelInfo;
-
-public:
-	void SetLevel(int level)
-	{
-		m_LogLevel=level;
-	}
-	void Error(const char* message)
-	{
-		if(m_LogLevel>=LogLevelError)
-			cout<<"[ERROR]: "<<message <<endl;
+	public:
+		const int LogLevelError = 0;
+		const int LogLevelWarning = 1;
+		const int LogLevelInfo = 2;
+		
+	private:
+		int m_LogLevel=LogLevelInfo ;
+		
+	public :
+		
+		void SetLevel(int level)
+		{
+			m_LogLevel = level;
+		}
+		
+		void Error(const char message)
+		{
+			if(m_LogLevel>=LogLevelError)
 			
-	}
-	void Warn(const char* message)
-	{
-		if(m_LogLevel>=LogLevelWarning)
-			cout<<"[WARNING]: "<<message <<endl;
-	}
-	void Info(const char* message)
-	{
-		if(m_LogLevel>=LogLevelInfo)
-			cout<<"[INFO]: "<<message <<endl;
-	}
-	
+			cout<<"[ERROR]:"<<message<<endl;
+		}
+		
+		void Warn(const char* message)
+		{
+			if(m_LogLevel>=LogLevelWarning)
+			
+			cout<<"[WARNING]:"<<message<<endl;
+		}
+		
+		void Info(const char* message)
+		{
+			if(m_LogLevel>=LogLevelInfo)
+			
+			cout<<"[INFO]:"<<message<<endl;
+		}
+		
 };
 
-int main()
 
-{
-Log oLog;
-oLog.LogLevelWarning;
-oLog.Warn("HELLO!!!");
-	
-std::cin.get();
-	
-}
+   int main()
+   {
+   	Log oLog;
+   	oLog.SetLevel(oLog.LogLevelWarning);
+   	oLog.Warn("Hello!!!");
+   	
+   	std::cin.get();
+   }
